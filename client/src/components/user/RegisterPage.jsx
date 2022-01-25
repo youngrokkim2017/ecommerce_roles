a8e9a0017db58f124d837af03d0b585e14fca525
Patrick Kim <patrick.yrkim@gmail.com>
2022-01-24 23:04:09 -0800

    Update register form

--- client/src/components/user/RegisterPage.jsx
+++ client/src/components/user/RegisterPage.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Row, Col, Container, Form, Button } from 'react-bootstrap'
 
-const RegisterPage = () => {
+const INITIAL_FORM = {
+    username: '',
+    password: ''
+}
+
+function RegisterPage() {
+    const [form, setForm] = useState(INITIAL_FORM)
+
+    const updateFormValue = (key) => (e) => {
+        setForm({
+            ...form, 
+            [key]: e.currentTarget.value
+        })
+    }
+
+    const registerUser = async (e) => {
+        e.preventDefault()
+
+        await axios.post("http://localhost:5000/register", form)
+
+        setForm(INITIAL_FORM)
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row>
+            <Col></Col>
+            <Col xs={6}>
+                <h1>Register</h1>
+                <Form onSubmit={registerUser}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        // type="text" 
+                        value={form.username} 
+                        onChange={updateFormValue('username')}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        value={form.password} 
+                        onChange={updateFormValue('password')}
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                </Form>
+            </Col>
+            <Col></Col>
+            </Row>
+        </Container>
     )
 }
 
 export default RegisterPage
+
+
+// import React from 'react'
+
+// const RegisterPage = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// export default RegisterPage
